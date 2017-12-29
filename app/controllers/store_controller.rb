d6ed6c5845c662a44e601bbe2cc31934ed028913
CrazedCoder <okcowboy80@yahoo.com>
2017-12-28 23:02:06 -0600

    Did a lot of stuff

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,16 @@
 class StoreController < ApplicationController
+  
   def index
   	@products = Product.order(:title)
+  	@count = increment_counter
+  	@show_message = "Count: #{@count}" if @count > 5 
   end
+
+  def increment_counter
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+    session[:counter] += 1
+  end
+
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -9,6 +9,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
       image_url:    'lorem.jpg',
       price:        19.95
     }
+
   end
 
   test "should get index" do
@@ -43,6 +44,14 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should update product" do
     patch product_url(@product), params: { product: @update }
     assert_redirected_to product_url(@product)
+  end
+
+  test "can't delete product in cart" do
+      assert_difference('Product.count', 0) do
+        delete product_url(products(:two))
+      end
+      
+      assert_redirected_to products_url
   end
 
   test "should destroy product" do
